c4e2b9179e8ea976f4b6dd9ac8db868d6f784f8d
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-12 23:30:59 +0100

    refactoring into smaller methods

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -11,10 +11,6 @@ import java.util.stream.Stream;
 public class Main {
   
   public static void main(String[] args) {
-  
-    // Jane [America/New_York] : Wednesday, June 14, 2023, 5:00 PM
-    // <---> Joe [Australia/Sydney] : Thursday, 125 June 2023, 7:00 am
-    
     System.setProperty("user.timezone", "America/New_York");
     Locale.setDefault(Locale.US);
     
@@ -22,11 +18,6 @@ public class Main {
     ZoneId zoneSydney = ZoneId.of("Australia/Sydney");
     
     ZonedDateTime zdtNow = ZonedDateTime.now().withMinute(0).withSecond(0);
-    ZonedDateTime zdtNowNY = zdtNow.withZoneSameInstant(zoneNewYork);
-    System.out.println(zdtNowNY);
-    ZonedDateTime zdtNowSY = zdtNow.withZoneSameInstant(zoneSydney);
-    System.out.println(zdtNowSY);
-    
     DateTimeFormatter dtfNY =
       DateTimeFormatter.ofPattern("EEEE, MMMM d yyyy, h:mm a", Locale.US);
     DateTimeFormatter dtfSY =
@@ -34,26 +25,30 @@ public class Main {
         Locale.of("en", "AU"));
     
     Stream.iterate(zdtNow, zdt -> zdt.isBefore(zdtNow.plusDays(10)), dt -> dt.plusHours(1))
-      .filter(zdt -> {
-        ZonedDateTime zdtNY = zdt.withZoneSameInstant(zoneNewYork);
-        ZonedDateTime zdtSY = zdt.withZoneSameInstant(zoneSydney);
-        for (var z : List.of(zdtNY, zdtSY)) {
-          if (z.getDayOfYear() == zdtNow.withZoneSameInstant(z.getZone()).getDayOfYear()
-                || z.getDayOfWeek() == DayOfWeek.SATURDAY || z.getDayOfWeek() == DayOfWeek.SUNDAY
-          || z.getHour() < 7 || z.getHour() > 20
-                || ( z.getHour() == 20 && (z.getMinute() > 0 || z.getSecond() > 0))) {
-            return false;
-          }
-        }
-        return true;
-      })
-//      .limit(5)
-      .forEach(zdt -> {
-        ZonedDateTime zdtNY = zdt.withZoneSameInstant(zoneNewYork).withMinute(0);
-        ZonedDateTime zdtSY = zdt.withZoneSameInstant(zoneSydney).withMinute(0);
-        System.out.println(
-          "Jane [" + zdtNY.getZone() + "] : " + zdtNY.format(dtfNY)
-            + " <---> Joe [" + zdtSY.getZone() + "] : " + zdtSY.format(dtfSY));
-      });
+      .filter(zdt -> isValidTime(zdt, zdtNow, zoneNewYork, zoneSydney))
+      .forEach(zdt -> printFormattedDate(zdt, zoneNewYork, zoneSydney, dtfNY, dtfSY));
+  }
+  
+  private static boolean isValidTime(ZonedDateTime zdt, ZonedDateTime zdtNow, ZoneId zoneNewYork,
+                                     ZoneId zoneSydney) {
+    ZonedDateTime zdtNY = zdt.withZoneSameInstant(zoneNewYork);
+    ZonedDateTime zdtSY = zdt.withZoneSameInstant(zoneSydney);
+    for (var z : List.of(zdtNY, zdtSY)) {
+      if (z.getDayOfYear() == zdtNow.withZoneSameInstant(z.getZone()).getDayOfYear()
+            || z.getDayOfWeek() == DayOfWeek.SATURDAY || z.getDayOfWeek() == DayOfWeek.SUNDAY
+            || z.getHour() < 7 || z.getHour() > 20
+            || (z.getHour() == 20 && (z.getMinute() > 0 || z.getSecond() > 0))) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  private static void printFormattedDate(ZonedDateTime zdt, ZoneId zoneNewYork, ZoneId zoneSydney
+    , DateTimeFormatter dtfNY, DateTimeFormatter dtfSY) {
+    ZonedDateTime zdtNY = zdt.withZoneSameInstant(zoneNewYork).withMinute(0);
+    ZonedDateTime zdtSY = zdt.withZoneSameInstant(zoneSydney).withMinute(0);
+    System.out.println("Jane [" + zdtNY.getZone() + "] : " + zdtNY.format(dtfNY)
+                         + " <---> Joe [" + zdtSY.getZone() + "] : " + zdtSY.format(dtfSY));
   }
 }
---
+++ src/dev/lpa/Solution.java
@@ -0,0 +1,9 @@
+package dev.lpa;
+
+public class Solution {
+  
+  public static void main(String[] args) {
+  
+  
+  }
+}
